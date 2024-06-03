83cbd5121781d111e9cab4e2bdc15cba32bfff1b
Iyusei035 <130746264+Iyusei035@users.noreply.github.com>
2024-06-03 14:57:02 +0900

    斬撃作成

--- Assets/Script/Game/PlayerAttack/Arcana_SnowShield/Arcana_SnowShieldAction.cs
+++ Assets/Script/Game/PlayerAttack/Arcana_SnowShield/Arcana_SnowShieldAction.cs
@@ -9,6 +9,6 @@ public class Arcana_SnowShieldAction : ArcanaBase
         _prefab = Resources.Load<GameObject>("SnowShieldEffect/SnowShield");
         _normalEffect = Instantiate(_prefab, _pos, Quaternion.identity);
         _normalEffect.gameObject.SetActive(true);
-        Destroy(_normalEffect.gameObject, 10.0f);
+        Destroy(_normalEffect.gameObject, 2.0f);
     }
 }
